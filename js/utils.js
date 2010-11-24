876ff53dea97b2c2f6e7011102da6ffc4e2e57fc
David DeSandro <david@desandro.com>
2010-11-24 06:59:54 -0500

    Add range to utils

--- js/utils.js
+++ js/utils.js
@@ -18,3 +18,59 @@ Element.prototype.removeClassName = function (b) {
 Element.prototype.toggleClassName = function (a) {
     this[this.hasClassName(a) ? "removeClassName" : "addClassName"](a)
 };
+
+// ======================= slider =============================== //
+
+var Tangibles = {};
+Tangibles.isTangible = !!('createTouch' in document);
+Tangibles.CursorStartEvent = Tangibles.isTangible ? 'touchstart' : 'mousedown';
+Tangibles.CursorMoveEvent = Tangibles.isTangible ? 'touchmove' : 'mousemove';
+Tangibles.CursorEndEvent = Tangibles.isTangible ? 'touchend' : 'mouseup';
+
+function ProxyRange (el) {
+  this.element = el;
+  this.width = this.element.offsetWidth;
+  this.slider = this.element.children[0];
+  
+  this.x = this.element.offsetLeft;
+  
+  this.slider.addEventListener( Tangibles.CursorStartEvent, this, false );
+};
+
+ProxyRange.prototype.handleEvent = function(event) {
+  if ( this[event.type] ) {
+    this[event.type](event);
+  }
+};
+
+ProxyRange.prototype[ Tangibles.CursorStartEvent ] = function(event) {
+  this.element.addClassName('highlighted');
+  
+  window.addEventListener( Tangibles.CursorMoveEvent, this, false );
+  window.addEventListener( Tangibles.CursorEndEvent, this, false );
+  
+  event.preventDefault();
+};
+
+ProxyRange.prototype[ Tangibles.CursorMoveEvent ] = function(event) {
+  
+  var cursor = Tangibles.isTangible ? event.touches[0] : event,
+      x = cursor.pageX - this.x;
+  x = Math.max( 0, Math.min( this.width, x ) );
+  
+  this.slider.style.webkitTransform = 'translate3d(' + x + 'px,0,0)';
+  
+  
+  event.preventDefault();
+};
+
+ProxyRange.prototype[ Tangibles.CursorEndEvent ] = function(event) {
+  
+  this.element.removeClassName('highlighted');
+  
+  window.removeEventListener( Tangibles.CursorMoveEvent, this, false );
+  window.removeEventListener( Tangibles.CursorEndEvent, this, false );
+};
+
+
+
