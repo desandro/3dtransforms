3dd8e442673a4d80e2cef78379ae699a5ed1edb0
David DeSandro <david@desandro.com>
2010-11-28 16:30:56 -0500

    standardize cube and box demo

---
+++ js/rotate-box.js
@@ -0,0 +1,20 @@
+var init = function() {
+  var box = document.getElementById('box'),
+      showPanelButtons = document.querySelectorAll('#show-buttons button'),
+      panelClassName = 'show-1';
+
+  for (var i=0, len = showPanelButtons.length; i < len; i++) {
+    showPanelButtons[i].addEventListener( 'click', function( event ){
+      box.removeClassName( panelClassName );
+      panelClassName = event.target.className;
+      box.addClassName( panelClassName );
+    }, false);
+  }
+  
+  document.getElementById('toggle-backface-visibility').addEventListener( 'click', function(){
+    box.toggleClassName('panels-backface-invisible');
+  }, false);
+  
+};
+  
+window.addEventListener( 'DOMContentLoaded', init, false);
